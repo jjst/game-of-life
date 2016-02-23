043ab74f6cce0fef8c60c646a94baf96266ee7f0
Jeremie Jost <jeremiejost@gmail.com>
2016-02-23 19:10:20 +0000

    Add is_neighbour() function

--- gameoflife.py
+++ gameoflife.py
@@ -1,4 +1,10 @@
 
+def neighbours(cell):
+    x, y = cell
+    return [(x-1, y-1), (x, y-1), (x+1, y-1),
+            (x-1, y),             (x+1, y),
+            (x-1, y+1), (x, y+1), (x+1, y+1)]
+
 class GameOfLife(object):
 
     def __init__(self, alive_cells=[]):
--- test_gameoflife.py
+++ test_gameoflife.py
@@ -1,4 +1,5 @@
-from gameoflife import GameOfLife
+from nose.tools import *
+from gameoflife import *
 
 def test_is_alive_alive_cell():
     game_of_life = GameOfLife(alive_cells=[(1,2),(3,4)])
@@ -8,3 +9,11 @@ def test_is_alive_alive_cell():
 def test_is_alive_dead_cell():
     game_of_life = GameOfLife(alive_cells=[(1,2),(3,4)])
     assert game_of_life.is_alive((1,1)) == False
+
+def test_neighbours():
+    assert_items_equal(
+        neighbours((1,1)),
+        [(0,0), (1,0), (2,0),
+         (0,1),        (2,1),
+         (0,2), (1,2), (2,2)]
+    )

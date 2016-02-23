bc299c4f55464b37b31c8ddee6aca4e58a7c8e1a
Jeremie Jost <jeremiejost@gmail.com>
2016-02-23 21:54:08 +0000

    Implement next_step() function

--- gameoflife.py
+++ gameoflife.py
@@ -6,13 +6,16 @@ def neighbours(cell):
             (x-1, y),             (x+1, y),
             (x-1, y+1), (x, y+1), (x+1, y+1)]
 
-class GameOfLife(object):
+class GameOfLifeStep(object):
 
     def __init__(self, alive_cells=[]):
         self.alive_cells = alive_cells
 
     def is_alive(self, cell):
         return cell in self.alive_cells
+
+    def is_dead(self, cell):
+        return cell not in self.alive_cells
 
     def alive_neighbours(self, cell):
         return [neighbour for neighbour in neighbours(cell)
@@ -28,3 +31,10 @@ class GameOfLife(object):
         all_neighbours = set(itertools.chain.from_iterable(
             [neighbours(cell) for cell in self.alive_cells]))
         return all_neighbours - set(self.alive_cells)
+
+    def next_step(self):
+        still_alive = [cell for cell in self.alive_cells
+                       if 2 <= len(self.alive_neighbours(cell)) <= 3]
+        newborns = [cell for cell in self.zombie_cells()
+                    if len(self.alive_neighbours(cell)) == 3]
+        return GameOfLifeStep(alive_cells=still_alive + newborns)
--- test_gameoflife.py
+++ test_gameoflife.py
@@ -3,13 +3,21 @@ from nose.tools import *
 from gameoflife import *
 
 def test_is_alive_alive_cell():
-    game_of_life = GameOfLife(alive_cells=[(1,2),(3,4)])
-    assert game_of_life.is_alive((1,2)) == True
-    assert game_of_life.is_alive((3,4)) == True
+    step = GameOfLifeStep(alive_cells=[(1,2),(3,4)])
+    assert step.is_alive((1,2)) == True
+    assert step.is_alive((3,4)) == True
 
 def test_is_alive_dead_cell():
-    game_of_life = GameOfLife(alive_cells=[(1,2),(3,4)])
-    assert game_of_life.is_alive((1,1)) == False
+    step = GameOfLifeStep(alive_cells=[(1,2),(3,4)])
+    assert step.is_alive((1,1)) == False
+
+def test_is_dead_alive_cell():
+    step = GameOfLifeStep(alive_cells=[(1,2),(3,4)])
+    assert step.is_dead((1,2)) == False
+
+def test_is_dead_dead_cell():
+    step = GameOfLifeStep(alive_cells=[(1,2),(3,4)])
+    assert step.is_dead((1,1)) == True
 
 def test_neighbours():
     assert_items_equal(
@@ -20,21 +28,50 @@ def test_neighbours():
     )
 
 def test_alive_neighbours():
-    game_of_life = GameOfLife(alive_cells=[(1,2),(2,0),(3,2)])
+    step = GameOfLifeStep(alive_cells=[(1,2),(2,0),(3,2)])
     assert_items_equal(
-        game_of_life.alive_neighbours((1,1)),
+        step.alive_neighbours((1,1)),
         [(1,2),(2,0)]
     )
 
 def test_zombie_cells_are_neighbours():
-    game_of_life = GameOfLife(alive_cells=[(1,2),(2,0),(3,2)])
-    all_neighbours = list(itertools.chain.from_iterable(
-        [neighbours((1,2)), neighbours((2,0)), neighbours((3,2))]
-    ))
-    assert all(zombie in all_neighbours
-               for zombie in game_of_life.zombie_cells())
+    step = GameOfLifeStep(alive_cells=[(1,2),(2,0),(3,2)])
+    all_neighbours = neighbours((1,2)) + neighbours((2,0)) + neighbours((3,2))
+    assert_items_equal(step.zombie_cells(), set(all_neighbours))
 
 def test_zombie_cells_are_all_dead():
-    game_of_life = GameOfLife(alive_cells=[(1,2),(2,0),(3,2)])
-    assert all(not game_of_life.is_alive(zombie)
-               for zombie in game_of_life.zombie_cells())
+    step = GameOfLifeStep(alive_cells=[(1,2),(1,1),(2,1)])
+    assert all(step.is_dead(zombie)
+               for zombie in step.zombie_cells())
+
+def test_next_step_cell_with_no_neighbours_dies():
+    step = GameOfLifeStep(alive_cells=[(1,2)])
+    next_step = step.next_step()
+    assert (1,2) not in next_step.alive_cells
+
+def test_next_step_cell_with_one_neighbours_dies():
+    step = GameOfLifeStep(alive_cells=[(1,2), (1,1)])
+    next_step = step.next_step()
+    assert (1,1) not in next_step.alive_cells
+    assert (1,2) not in next_step.alive_cells
+
+def test_next_step_cell_with_2_neighbours_survives():
+    step = GameOfLifeStep(alive_cells=[(0,1), (1,1), (2,1)])
+    next_step = step.next_step()
+    assert (1,1) in next_step.alive_cells
+
+def test_next_step_cell_with_3_neighbours_survives():
+    step = GameOfLifeStep(alive_cells=[(0,1), (1,1), (2,1), (1,0)])
+    next_step = step.next_step()
+    assert (1,1) in next_step.alive_cells
+
+def test_next_step_cell_with_over_3_neighbours_dies():
+    step = GameOfLifeStep(alive_cells=[(0,1), (1,1), (2,1), (1,0), (2,0)])
+    next_step = step.next_step()
+    assert (1,1) not in next_step.alive_cells
+
+def test_next_dead_cell_with_3_alive_neighbours_becomes_alive():
+    step = GameOfLifeStep(alive_cells=[(0,1), (1,1), (2,1)])
+    next_step = step.next_step()
+    assert_in((1,0), next_step.alive_cells)
+    assert_in((1,2), next_step.alive_cells)
